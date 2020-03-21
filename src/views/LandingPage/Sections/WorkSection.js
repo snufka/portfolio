e6caacc5f37f0c601f738385a9c0e5c52dc6c562
snufka <52801250+snufka@users.noreply.github.com>
2020-03-21 22:41:07 +0100

    contact form updated

--- src/views/LandingPage/Sections/WorkSection.js
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -20,12 +20,9 @@ export default function WorkSection() {
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem cs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Work with us</h2>
+          <h2 className={classes.title}>Let{"'"}s Stay in Touch</h2>
           <h4 className={classes.description}>
-            Divide details about your product or agency work into parts. Write a
-            few lines about each one and contact us about any further
-            collaboration. We will responde get back to you in a couple of
-            hours.
+            Would love to hear your tought and feedback. Do not hesitated to ping me.
           </h4>
           <form>
             <GridContainer>
